866dec3a5fc7270db1066554f50973dd6f25275a
csibley20 <43002599+csibley20@users.noreply.github.com>
2018-10-01 08:32:33 -0400

    Add files via upload

---
+++ src/OpeartionsPractice.java
@@ -0,0 +1,25 @@
+
+public class OpeartionsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number;
+		number = 20;
+		System.out.println("Number: " + number);
+		number += 5;
+		System.out.println("Add 5: " + number);
+		number /= 2;
+		System.out.println("Divide by 2: " + number);
+		number *= 4;
+		System.out.println("Multiply by 4: " + number);
+		number -= 6;
+		System.out.println("Subtract 6: " + number);
+		number %= 2;
+		System.out.println("Modular Division by 2: " + number);
+		
+		
+		
+	}
+
+}
